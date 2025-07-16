6f07e98ebb3a2e426f649362af7dba27d26b1a71
AsadillohTemirov-05 <asadillohdeveloper05@gmail.com>
2025-07-16 15:47:23 +0300

    Problem Solved

--- src/redux/weatherSlice.js
+++ src/redux/weatherSlice.js
@@ -2,6 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from "axios";
 
 const API_KEY=import.meta.env.VITE_WEATHER_API_KEY;
+console.log(API_KEY);
 
 const BASE_URL="http://api.weatherapi.com/v1";
 
